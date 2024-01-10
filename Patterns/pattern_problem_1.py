f00e70a7303d56d90266df9b8c84c4ee6b421465
Yuvilius <yuvikatwal187@gmail.com>
2024-01-10 23:49:44 +0530

    Add Patterns directory

---
+++ Patterns/pattern_problem_1.py
@@ -0,0 +1,13 @@
+# This is the first pattern problem in the series.
+
+# This problem is called the "Solid Square" pattern problem.
+
+# Solution :
+
+n = 5  # Arbitrary Value of n is taken
+
+for i in range(n):
+    for j in range(n):
+        print("*", end="")
+    print()
+    
