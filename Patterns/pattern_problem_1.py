d35ed2b26967cd9439ead8c7dc8debca8342db49
Yuvilius <yuvikatwal187@gmail.com>
2024-01-10 23:52:58 +0530

    First Pattern Problem

--- Patterns/pattern_problem_1.py
+++ Patterns/pattern_problem_1.py
@@ -8,6 +8,5 @@ n = 5  # Arbitrary Value of n is taken
 
 for i in range(n):
     for j in range(n):
-        print("*", end="")
+        print("*", end="")   # 'end' is a python parameter used to get the output in a single line
     print()
-    
