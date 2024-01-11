899a3008ded8d58aca51abb788440283625177b4
Yuvilius <yuvikatwal187@gmail.com>
2024-01-11 20:51:08 +0530

    2nd Pattern solved

---
+++ Patterns/pattern_problem_2.py
@@ -0,0 +1,10 @@
+# This is the second pattern problem in the series
+
+# This problem is called the " Right-angled triangle pattern "
+
+n = 5  # Arbitrary value of n is taken
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end="")
+    print("")
