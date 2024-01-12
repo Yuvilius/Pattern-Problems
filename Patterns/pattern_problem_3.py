2e78ba863822c05091e31217d8b57525f651e1b7
Yuvilius <yuvikatwal187@gmail.com>
2024-01-12 19:58:38 +0530

    3rd pattern problem solved

---
+++ Patterns/pattern_problem_3.py
@@ -0,0 +1,10 @@
+# This is the third pattern problem in the series
+
+# This problem is called the " Right-angled Number Triangle "
+
+n = 5  # Arbitrary value of n is taken
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print("")
