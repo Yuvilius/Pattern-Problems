c393813f1b1cae700f04be9f13ebc67f7d6883c5
Yuvilius <yuvikatwal187@gmail.com>
2024-01-13 22:43:14 +0530

    Solve 4th pattern

---
+++ Patterns/pattern_problem_4.py
@@ -0,0 +1,12 @@
+# This is the fourth pattern problem in the series
+
+# This problem is called the " Right-angled Repeated-Number Triangle "
+
+
+n = 5  # Arbitrary value of 5 is taken
+
+for i in range(1, n+1):
+    for j in range(1, i + 1):
+        print(i,end='')
+    print(" ")
+
