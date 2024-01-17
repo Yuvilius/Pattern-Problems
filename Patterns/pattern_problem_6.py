9c74068c62b5b6b8ee565fe6ff138dca582b311d
Yuvilius <yuvikatwal187@gmail.com>
2024-01-17 22:11:31 +0530

    6th pattern solved

--- Patterns/pattern_problem_6.py
+++ Patterns/pattern_problem_6.py
@@ -1,10 +1,18 @@
 # This is the 6th pattern problem in the series
 
+# This pattern is called "Inverted Right Angled Number Triangle"
 
-n = 5
+n = 5  # We take an arbitrary value of n
 
 for i in range(n):
     for j in range(1, n-i+1):
         print(j, end="")
     print("")
 
+# Expected Outcome:
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
+# for n = 5
