d840243e4c2500b8bdbe7123777ca8548692013c
Yuvilius <yuvikatwal187@gmail.com>
2024-01-17 22:13:22 +0530

    write comments for 5th pattern problem

--- Patterns/pattern_problem_5.py
+++ Patterns/pattern_problem_5.py
@@ -2,9 +2,18 @@
 
 # This problem is called "Inverted Right Angle Triangle" Pattern
 
-n = 5
+n = 5  # We take an arbitrary value of n
 
 for i in range(n):
     for j in range(n-i):
         print("*", end="")
     print("")
+
+# Expected Outcome:
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# for n = 5
+
