95c9663f357b02562d552229939f80136d7470e3
Yuvilius <yuvikatwal187@gmail.com>
2024-01-20 19:37:59 +0530

    Write comments for 8th pattern

--- Patterns/pattern_problem_8.py
+++ Patterns/pattern_problem_8.py
@@ -1,15 +1,25 @@
 # This is the 8th pattern problem in the series
 
+# Based on the last problem, we can call this the "Inverted Triangle Pattern"
+
 n = 5  # We take an arbitrary value for n
 
 for i in range(n):
-    for x in range(i):
+    for x in range(i):                # Logic for printing the spaces in the pattern
         print(' ', end='')
 
-    for y in range(2*n - (2*i + 1)):
+    for y in range(2*n - (2*i + 1)):  # Logic for printing the stars in the pattern
         print("*", end='')
 
     for z in range(i):
-        print(' ', end='')
+        print(' ', end='')            # Logic for printing the spaces in the pattern
 
     print("")
+
+# Expected Output:
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+# for n = 5
