f4fd43adae96c023e5730beb47dc27ff38dc8ed5
Yuvilius <yuvikatwal187@gmail.com>
2024-01-20 19:32:28 +0530

    Write commits for th 7th pattern

--- Patterns/pattern_problem_7.py
+++ Patterns/pattern_problem_7.py
@@ -1,16 +1,25 @@
 # This is the 7th pattern problem in the series
 
+# We can call this problem simply as the "Triangle Pattern"
+
 n = 5  # We take an arbitrary value for n
 
 for i in range(n):
-    for x in range(n - i - 1):
+    for x in range(n - i - 1):  # Logic for printing the spaces in the pattern
         print(" ", end='')
 
-    for y in range(2*i + 1):
+    for y in range(2*i + 1):    # Logic for printing the stars in the pattern
         print("*", end='')
 
-    for z in range(n - i - 1):
+    for z in range(n - i - 1):  # Logic for printing the spaces in the pattern
         print(" ", end='')
 
     print("")
 
+# Expected Outcome:
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+# for n = 5
