ef117693abca5d7ca81a84c9cc15dec39c092c32
Yuvilius <yuvikatwal187@gmail.com>
2024-01-23 23:15:16 +0530

    11th Pattern Solved

--- Patterns/pattern_problem_11.py
+++ Patterns/pattern_problem_11.py
@@ -2,5 +2,14 @@
 
 n = 5  # We take an arbitrary value for n
 
+start = 1
+
 for i in range(n):
-    pass
+    if i % 2 == 0:
+        start = 1
+    else:
+        start = 0
+    for j in range(i+1):
+        print(start, end='')
+        start = 1 - start
+    print("")
