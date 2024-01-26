8cb3ff06573a69ad32feb0278ca7489c171d707b
Yuvilius <yuvikatwal187@gmail.com>
2024-01-26 22:52:00 +0530

    pattern 13 solved

--- Patterns/pattern_problem_13.py
+++ Patterns/pattern_problem_13.py
@@ -2,5 +2,10 @@
 
 n = 5  # We take an arbitrary value of n
 
-for i in range(n):
-    pass
+num = 1
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(num, end='')
+        num += 1
+    print("")
